b592947c1d4fa129e06be4f5ced3b11ed25c9e39
Jerome Infante <141633312+doofenzy@users.noreply.github.com>
2025-03-06 08:50:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
-# Python Program to convert prefix to Infix
-def prefixToInfix(prefix):
-    stack = []
-    
-    # read prefix in reverse order
-    i = len(prefix) - 1
-    while i >= 0:
-        if not isOperator(prefix[i]):
-            
-            # symbol is operand
-            stack.append(prefix[i])
-            i -= 1
+# Python program to convert infix to prefix
+
+# Check if character is an operator
+def isOperator(ch):
+    return ch in ['+', '-', '*', '/', '^']
+
+# Get precedence of operators
+def operatorPrecedence(op):
+    if op == '^':
+        return 3
+    if op in ['*', '/']:
+        return 2
+    if op in ['+', '-']:
+        return 1
+    return -1
+
+# Convert infix expression to postfix notation
+def convertInfixToPostfix(s):
+    st = []
+    res = ""
+    sz = len(s)
+
+    for i in range(sz):
+        if s[i].isalnum():
+            res += s[i]
+        elif s[i] == '(':
+            st.append(s[i])
+        elif s[i] == ')':
+            while st and st[-1] != '(':
+                res += st.pop()
+            st.pop()
         else:
-          
-            # symbol is operator
-            str = "(" + stack.pop() + prefix[i] + stack.pop() + ")"
-            stack.append(str)
-            i -= 1
-    
-    return stack.pop()
+            while st and operatorPrecedence(s[i]) <= operatorPrecedence(st[-1]):
+                res += st.pop()
+            st.append(s[i])
 
-def isOperator(c):
-    if c == "*" or c == "+" or c == "-" or c == "/" or c == "^" or c == "(" or c == ")":
-        return True
-    else:
-        return False
+    while st:
+        res += st.pop()
 
-# Driver code
-if __name__=="__main__":
-    str = "*-A/BC-/AKL"
-    print(prefixToInfix(str))
+    return res
+
+# Convert infix expression to prefix notation
+def convertToPrefix(infix):
+    infix = infix[::-1]
+    infix = ''.join(['(' if ch == ')' else ')' if ch == '(' else ch for ch in infix])
     
-# This code is contributed by avishekarora
+    postfix = convertInfixToPostfix(infix)
+    return postfix[::-1]
+
+if __name__ == "__main__":
+    s = "(a-b/c)*(a/k-l)"
+    print(convertToPrefix(s))

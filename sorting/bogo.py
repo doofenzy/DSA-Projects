3438cf8e5a7ae0787c3c75999831aa45ccc17479
Jerome Infante <jeromeinfante.dev@gmail.com>
2025-03-26 15:40:59 +0800

    added bogo sort

---
+++ sorting/bogo.py
@@ -0,0 +1,67 @@
+# import random
+#
+#
+#
+# def shuffle(a):
+#     n = len(a)
+#     for i in range(0, n):
+#         r = random.randint(0, n-1)
+#         a[i], a[r] = a[r], a[i]
+#
+# def is_sorted(a):
+#     n = len(a)
+#     for i in range(0, n-1):
+#         if (a[i] > a[i+1]):
+#             return False
+#     return True
+#
+# def bogoSort(a):
+#     n = len(a)
+#     while (is_sorted(a) == False):
+#         shuffle(a)
+#
+# a = [3, 2, 4, 1, 0, 5]
+# a = bogoSort(a)
+# print(a)
+
+
+# Python program for implementation of Bogo Sort
+import random
+
+
+# Sorts array a[0..n-1] using Bogo sort
+
+
+def bogoSort(a):
+    n = len(a)
+    while (is_sorted(a) == False):
+        shuffle(a)
+
+    # To check if array is sorted or not
+
+
+def is_sorted(a):
+    n = len(a)
+    for i in range(0, n - 1):
+        if (a[i] > a[i + 1]):
+            return False
+    return True
+
+
+# To generate permutation of the array
+
+
+def shuffle(a):
+    n = len(a)
+    for i in range(0, n):
+        r = random.randint(0, n - 1)
+        a[i], a[r] = a[r], a[i]
+
+    # Driver code to test above
+
+
+a = [23, 65 ,87, 98, 10, 23, 84, 63, 81, 90]
+bogoSort(a)
+print("Sorted array :")
+for i in range(len(a)):
+    print("%d" % a[i]),

84026551cf1a8163061cf0831305a72d1a1036cb
Jerome Infante <jeromeinfante.dev@gmail.com>
2025-03-26 15:43:39 +0800

    updated bogo sort

--- sorting/bogo.py
+++ sorting/bogo.py
@@ -1,30 +1,3 @@
-# import random
-#
-#
-#
-# def shuffle(a):
-#     n = len(a)
-#     for i in range(0, n):
-#         r = random.randint(0, n-1)
-#         a[i], a[r] = a[r], a[i]
-#
-# def is_sorted(a):
-#     n = len(a)
-#     for i in range(0, n-1):
-#         if (a[i] > a[i+1]):
-#             return False
-#     return True
-#
-# def bogoSort(a):
-#     n = len(a)
-#     while (is_sorted(a) == False):
-#         shuffle(a)
-#
-# a = [3, 2, 4, 1, 0, 5]
-# a = bogoSort(a)
-# print(a)
-
-
 # Python program for implementation of Bogo Sort
 import random
 
